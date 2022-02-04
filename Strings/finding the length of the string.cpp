11a1b189d5e66165cbfd47a8434259facd0e2298
AkshatSinghal123 <88577880+AkshatSinghal123@users.noreply.github.com>
2022-02-04 23:01:59 +0530

    finding the length of the string.cpp

---
+++ Strings/finding the length of the string.cpp
@@ -0,0 +1,11 @@
+//finding the length of the string
+#include<stdio.h>
+int main(){
+	char *s="Welcome";
+	int i;
+	for(i=0;s[i]!='\0';i++){
+		
+	}
+	printf("Length of the string is %d",i);
+	return 0; 
+}
